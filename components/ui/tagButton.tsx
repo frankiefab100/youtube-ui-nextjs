72d1dced37465e91e3b9473cd6c5e07b83bc21ed
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-02-14 01:37:45 +0100

    chore: update tagButton and add type definition

--- components/ui/tagButton.tsx
+++ components/ui/tagButton.tsx
@@ -1,19 +1,24 @@
+"use client";
 import { CATEGORY_TAGS } from "@/constants/categoryTags";
 import Link from "next/link";
 import { BiChevronRight } from "react-icons/bi";
 
-export const Tags: React.FC = () => {
+export const Tags: React.FC<{ id: number; label: string }> = () => {
   return (
     <div className="relative bg-backgroundAccent flex flex-row justify-evenly items-center px-6 py-3 max-w-full whitespace-nowrap overflow-hidden z-5">
-      {CATEGORY_TAGS.map((tag, index) => {
+      {CATEGORY_TAGS.map((tag) => {
         return (
           <Link
-            key={index}
+            key={tag.id}
             href="#"
             role="button"
-            className="first-of-type:bg-colorPrimary first-of-type:text-black first-of-type:border-none text-colorPrimary bg-backgroundAccent border border-colorSecondary px-3 py-1 rounded-full text-sm font-medium mr-4"
+            className={`${
+              tag.id === 1
+                ? "bg-colorPrimary text-black border-none"
+                : "bg-backgroundAccent text-colorPrimary border border-colorSecondary"
+            } px-3 py-1 rounded-full text-sm font-medium mx-2`}
           >
-            {tag}
+            {tag.label}
           </Link>
         );
       })}
